bf1cf4ffdd4f40efd4fbf19491e5898a09ae2744
Netaly Klemann <netalyk@mail.tau.ac.il>
2023-01-07 14:17:10 +0200

    fixed fifo delete and start set

--- src/Cache/CacheFIFO.ts
+++ src/Cache/CacheFIFO.ts
@@ -1,48 +1,59 @@
-import { ICacheAlgo } from "inteface/ICacheAlgo";
-import { AbstractCacheAlgo } from "AbstractCacheAlgo";
+import {ICacheAlgo} from "inteface/ICacheAlgo";
+import {AbstractCacheAlgo} from "AbstractCacheAlgo";
 
-// class CacheFIFO<K,V> extends AbstractCacheAlgo<K,V> implements ICacheAlgo<K,V> {
-//     #cacheStorage = new Map<K,V>();
-//     #keysQueueArray: [] = [];
+class CacheFIFO<K, V> extends AbstractCacheAlgo<K, V> implements ICacheAlgo<K, V> {
+    #cache = new Map<K, V>();
+    //first is the oldest key
+    #keysQueue: Array<K> = [];
+    #oldestKey: number = 0;
 
-//     // 50 just for now...
-//     #capacity: number = 50;
+    #isFull = () => this._capacity === this.#cache.size;
+
+    _capacity = 4;
+
+    getElement(key: K): V | undefined {
+        return this.#cache.get(key);
+    };
+
+    removeElement(key: K): boolean {
+        const idx: number = this.#keysQueue.indexOf(key);
+        if (idx > -1) {
+            this.#keysQueue.splice(idx, 1);
+            this.#cache.delete(key);
+            return true;
+        }
+        return false;
+    }
+
+    setElement(key: K, value: V): K | undefined {
+        let returnValue = undefined;
+        if (this.#cache.has(key)) {
+            //todo - not sure if need to remove or just update
+            returnValue = key;
+        } else if (this.#isFull()) {
+            if (this.#oldestKey === this._capacity) {
+                this.#oldestKey = 0;
+            }
+            this.#keysQueue[this.#oldestKey] = key;
+            this.#oldestKey += 1;
+        }
+        this.#cache.set(key, value);
+        return returnValue;
+    }
+
+}
 
 
-//     getElement(key: K): V | undefined {
-//         const result = this.#cacheStorage.get(key);
-//         return result;
-//     };
+const test = new Map()
 
-//     // not sure i need a key here
-//     removeElement (key: K): boolean {
-//         const KEY = this.#keysQueueArray[0];
-//         if (KEY) {
-//             this.#keysQueueArray.splice(0, 1);
-//             this.#cacheStorage.delete(KEY);
-//             return true;
-//         }
-//         return false;
-//     };
+test.set('a', 1)
+test.set('b', 2)
+test.set('c', 3)
+test.set('d', 4)
+test.delete('d')
 
-//     setElement (key: K, value: V): K | undefined {
-//         if (this.#keysQueueArray.length > this.#capacity)
-//         return
-//     };
-    
-// };
+console.log(test)
 
-
-// const test = new Map()
-
-// test.set('a', 1)
-// test.set('b', 2)
-// test.set('c', 3)
-// test.set('d', 4)
-// test.delete('d')
-
-// console.log(test)
-
-// const testArray = [1,2,3]
-// const argument = testArray.splice(1,1)
-// console.log(argument[0])
+const testArray = [1, 2, 3]
+const argument = testArray.splice(1, 1)
+console.log(argument[0])
--- src/Cache/CacheRandom.ts
+++ src/Cache/CacheRandom.ts
@@ -1,48 +1,45 @@
-import { ICacheAlgo } from "inteface/ICacheAlgo";
-import { AbstractCacheAlgo } from "AbstractCacheAlgo";
+import {ICacheAlgo} from "inteface/ICacheAlgo";
+import {AbstractCacheAlgo} from "../AbstractCacheAlgo";
 
-class CacheRandom<K,V> extends AbstractCacheAlgo<K,V> implements ICacheAlgo<K,V> {
-    #cacheStorage = new Map<K,V>();
-    #keysQueueArray: [] = [];
+class CacheRandom<K, V> extends AbstractCacheAlgo<K, V> implements ICacheAlgo<K, V> {
+    #cacheStorage = new Map<K, V>();
+    #keysArray: Array<K> = [];
 
     // 50 just for now...
-    #capacity: number = 50;
+    _capacity = 50;
 
-    isFull() {return this.#cacheStorage.size === this.#capacity}
+    isFull() {
+        return this.#cacheStorage.size === this._capacity
+    }
 
     getElement(key: K): V | undefined {
-        const result = this.#cacheStorage.get(key);
-        return result;
+        return this.#cacheStorage.get(key);
     };
 
 
-    removeElement (key: K): boolean {
-        const idx: number = this.#keysQueueArray.indexOf(key);
+    removeElement(key: K): boolean {
+        const idx: number = this.#keysArray.indexOf(key);
         if (idx > -1) {
-            this.#keysQueueArray.splice(idx, 1);
+            this.#keysArray.splice(idx, 1);
             this.#cacheStorage.delete(key);
             return true;
         }
         return false;
     };
 
-    setElement (key: K, value: V): K | undefined {
+    setElement(key: K, value: V): K | undefined {
+        let returnValue = undefined;
         if (this.#cacheStorage.has(key)) {
             this.removeElement(key);
-            this.setElement(key, value);
-            return key
-        };
-
-        if (!this.isFull()) {
-            this.#cacheStorage.set(key, value);
-            this.#keysQueueArray.push(key);
-        };
-
-        const randomIdxPick = Math.floor(Math.random()*this.#capacity);
-        const keypicked = this.#keysQueueArray[randomIdxPick];
-        this.removeElement(keypicked);
-
-        this.setElement(key, value);
-    };
-    
-};
+            returnValue = key;
+        } else if (this.isFull()) {
+            const randomIdxPick = Math.floor(Math.random() * this._capacity);
+            const keyPicked = this.#keysArray[randomIdxPick];
+            this.removeElement(keyPicked);
+            returnValue = keyPicked;
+        }
+        this.#cacheStorage.set(key, value);
+        this.#keysArray.push(key);
+        return returnValue;
+    }
+}
