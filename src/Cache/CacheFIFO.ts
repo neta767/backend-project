6f3adb59c0dcead05e976ad4da21d2c74f54d8cb
Netaly Klemann <netalyk@mail.tau.ac.il>
2023-01-14 15:14:34 +0200

    something

--- src/Cache/CacheFIFO.ts
+++ src/Cache/CacheFIFO.ts
@@ -1,25 +1,31 @@
 import {ICacheAlgo} from "inteface/ICacheAlgo";
-import {AbstractCacheAlgo} from "AbstractCacheAlgo";
+import {AbstractCacheAlgo} from "../AbstractCacheAlgo";
 
 class CacheFIFO<K, V> extends AbstractCacheAlgo<K, V> implements ICacheAlgo<K, V> {
-    #cache = new Map<K, V>();
-    //first is the oldest key
-    #keysQueue: Array<K> = [];
-    #oldestKey: number = 0;
 
-    #isFull = () => this._capacity === this.#cache.size;
+    //todo remove this-
+    public cache = new Map<K, V>();
+    // //first is the oldest key
+    public keysQueue: Array<K> = [];
+    public oldestKey: number = 0;
+    // cache = new Map<K, V>();
+    // //first is the oldest key
+    // keysQueue: Array<K> = [];
+    // oldestKey: number = 0;
+
+    isFull = () => this._capacity === this.cache.size;
 
     _capacity = 4;
 
     getElement(key: K): V | undefined {
-        return this.#cache.get(key);
+        return this.cache.get(key);
     };
 
     removeElement(key: K): boolean {
-        const idx: number = this.#keysQueue.indexOf(key);
+        const idx: number = this.keysQueue.indexOf(key);
         if (idx > -1) {
-            this.#keysQueue.splice(idx, 1);
-            this.#cache.delete(key);
+            this.keysQueue.splice(idx, 1);
+            this.cache.delete(key);
             return true;
         }
         return false;
@@ -27,33 +33,21 @@ class CacheFIFO<K, V> extends AbstractCacheAlgo<K, V> implements ICacheAlgo<K, V
 
     setElement(key: K, value: V): K | undefined {
         let returnValue = undefined;
-        if (this.#cache.has(key)) {
+        if (this.cache.has(key)) {
             //todo - not sure if need to remove or just update
+            this.removeElement(key);
             returnValue = key;
-        } else if (this.#isFull()) {
-            if (this.#oldestKey === this._capacity) {
-                this.#oldestKey = 0;
-            }
-            this.#keysQueue[this.#oldestKey] = key;
-            this.#oldestKey += 1;
+        } else if (this.isFull() && this.oldestKey === this._capacity) {
+            this.oldestKey = 0;
         }
-        this.#cache.set(key, value);
+        this.keysQueue[this.oldestKey] = key;
+        this.oldestKey += 1;
+        this.cache.set(key, value);
         return returnValue;
     }
 
 }
 
-
-const test = new Map()
-
-test.set('a', 1)
-test.set('b', 2)
-test.set('c', 3)
-test.set('d', 4)
-test.delete('d')
-
-console.log(test)
-
-const testArray = [1, 2, 3]
-const argument = testArray.splice(1, 1)
-console.log(argument[0])
+const t = new CacheFIFO();
+t.setElement(1, 1);
+console.log(t.cache, t.oldestKey, t.keysQueue);
